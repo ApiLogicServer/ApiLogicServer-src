ddb069ec24a5d8cd5a2d60c9248e78b2333cfe7d
Val Huber <valjhuber@gmail.com>
2023-07-05 21:20:08 -0700

    911 - docker fix

--- api_logic_server_cli/project_prototype/.devcontainer/setup.sh
+++ api_logic_server_cli/project_prototype/.devcontainer/setup.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$CODESPACE_NAME" ]
 then
-    echo "Codespace ready"
+    echo "devcontainer ready with defined ports"
 else
     echo ".devcontainer/setup: port creation..."
     gh codespace ports visibility 5656:public -c $CODESPACE_NAME
