2a1f00279d843e3111be52b4cfc21efb2d44a2d0
Val Huber <valjhuber@gmail.com>
2023-07-13 07:30:20 -0700

    docker compose place holder

--- api_logic_server_cli/project_prototype/devops/docker/run_image.sh
+++ api_logic_server_cli/project_prototype/devops/docker/run_image.sh
@@ -13,5 +13,5 @@
 # Start container and run the app
 docker run --env-file devops/docker/env.list -it --name api_logic_project --rm --net dev-network -p 5656:5656 -p 5002:5002 apilogicserver/apilogicserver_project_name_lower
 
-# Start container and run the app using docker compose
-docker compose run
+# Start container and run the app using docker compose  (FIXME - wip)
+# docker compose run -f docker-compose.yml
