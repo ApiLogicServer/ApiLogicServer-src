f503d5fef66326731fccbc329960a49125b5adc1
Val Huber <valjhuber@gmail.com>
2023-08-01 22:02:20 -0700

    acquire web app

---
+++ api_logic_server_cli/project_prototype/devops/docker-compose/install-webapp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# obtain the web directories
+
+if [ -d "etc" ] 
+then
+    echo "\n... starting\n"
+else
+    echo "\nPlease cd to <project>/devops/docker-compose - see readme\n" 
+    exit 1
+fi
+
+read -p "Ready to obtain web app files, press ENTER to proceed> "
+
+curl https://github.com/thomaxxl/safrs-react-admin/releases/download/0.1.2/safrs-react-admin-0.1.2.zip -LO
+echo "unzipping sra to build.."
+set +x
+unzip safrs-react-admin-0.1.2.zip
+set -x
+
+echo "\nrenaming to www"
+mv build www
