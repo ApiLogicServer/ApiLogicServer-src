d80a18d43a376a1095ee063d685e136c7a210748
Tyler Band <tylerm007@gmail.com>
2023-10-25 11:38:37 -0400

    rework budget SQL

--- tests/test_databases/mysql_test_databases/BudgetApp.sql
+++ tests/test_databases/mysql_test_databases/BudgetApp.sql
@@ -1,15 +1,15 @@
 --   ApiLogicServer create --db_url=mysql+pymysql://root:p@localhost:3306/BudgetApp --project_name=BudgetApp
 
 -- Create the database for the budgeting application
-DROP DATABASE BudgetApp;
-CREATE DATABASE BudgetApp;
+-- DROP DATABASE BudgetApp2;
+-- CREATE DATABASE BudgetApp2;
 
 -- Use the newly created database
-USE BudgetApp;
+-- USE BudgetApp2;
 
 -- Create the 'users' table to store user information - Multi-Tenant
-CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE tenant_user (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL
@@ -17,22 +17,22 @@ CREATE TABLE users (
 
 -- Create the 'accounts' table to store information about financial accounts
 CREATE TABLE accounts (
-    account_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
+    account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
     account_name VARCHAR(50) NOT NULL,
     balance DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES tenant_user(user_id)
 );
 
 -- Create the 'categories' table to store budget categories
 CREATE TABLE categories (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_id INTEGER PRIMARY KEY AUTOINCREMENT,
     category_name VARCHAR(50) NOT NULL,
-    is_expense TINYINT(1) NOT NULL DEFAULT 1  -- 1 for expense, 0 for income
+    is_expense INTEGER  NOT NULL DEFAULT 1  -- 1 for expense, 0 for income
 );
 
--- Insert sample data into the 'users' table
-INSERT INTO users (username, password, email)
+-- Insert sample data into the 'tenant_user' table
+INSERT INTO tenant_user (username, password, email)
 VALUES
     ('user1', 'password1', 'user1@example.com'),
     ('user2', 'password2', 'user2@example.com');
@@ -57,120 +57,89 @@ VALUES
 -- YEAR 2023 -    
 DROP TABLE IF EXISTS yr_total;
 CREATE TABLE yr_total (
-	year_id INT NOT NULL DEFAULT 2023,
-	user_id INT NOT NULL,
-    account_id INT NOT NULL,
-    category_id INT NOT NULL,
+    year_id INTEGER NOT NULL DEFAULT 2023,
+    user_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
     budget_total  DECIMAL(10, 2) NOT NULL,
-    actual_amount DECIMAL(10, 2) NOT NULL,
+    actual_amount DECIMAL(10, 2) NOT NULL DEFAULT 0,
     created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id),
+    FOREIGN KEY (user_id) REFERENCES tenant_user(user_id),
     FOREIGN KEY (category_id) REFERENCES categories(category_id),
-    PRIMARY KEY(user_id, category_id, account_id, year_id)
+    PRIMARY KEY(year_id, user_id, category_id)
 );
 
 -- Create 1,2,3,4 
 -- insert parent yr_total if not found
 DROP TABLE IF EXISTS qtr_total;
 CREATE TABLE qtr_total (
-    qtr_id TINYINT(1) NOT NULL,
-	year_id INT NOT NULL DEFAULT 2023,
-	user_id INT NOT NULL,
-    account_id INT NOT NULL,
-    category_id INT NOT NULL,
+    qtr_id INTEGER NOT NULL,
+    year_id INTEGER NOT NULL DEFAULT 2023,
+    user_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
     budget_total  DECIMAL(10, 2) NOT NULL,
-    actual_amount DECIMAL(10, 2) NOT NULL,
+    actual_amount DECIMAL(10, 2) NOT NULL DEFAULT 0,
     created_date  DATETIME DEFAULT CURRENT_TIMESTAMP ,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id),
+    FOREIGN KEY (user_id) REFERENCES tenant_user(user_id),
     FOREIGN KEY (category_id) REFERENCES categories(category_id),
-    FOREIGN KEY (user_id, category_id, account_id, year_id) REFERENCES yr_total(user_id, category_id, account_id, year_id),
-    PRIMARY KEY(user_id, category_id, account_id, year_id, qtr_id)
+    FOREIGN KEY (year_id, user_id, category_id) REFERENCES yr_total(year_id, user_id, category_id),
+    PRIMARY KEY(year_id, qtr_id, user_id, category_id)
 );
 
 -- Months 1-12 JAN, FEB ... DEC
 -- insert parent qtr_total if not found
 DROP TABLE IF EXISTS month_total;
 CREATE TABLE month_total (
-    month_id TINYINT(1) NOT NULL,
+    month_id INTEGER  NOT NULL,
     month_str VARCHAR(20), -- formula(month_as_string, month_id) JAN, FEB
-    qtr_id TINYINT(1) NOT NULL,
-	year_id INT NOT NULL DEFAULT 2023,
-	user_id INT NOT NULL,
-    account_id INT NOT NULL,
-    category_id INT NOT NULL,
+    year_id INTEGER NOT NULL DEFAULT 2023,
+    qtr_id INTEGER  NOT NULL,
+    user_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
     budget_total  DECIMAL(10, 2) NOT NULL,
-    actual_amount DECIMAL(10, 2) NOT NULL,
+    actual_amount DECIMAL(10, 2) NOT NULL DEFAULT 0,
     created_date  DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id),
+    FOREIGN KEY (user_id) REFERENCES tenant_user(user_id),
     FOREIGN KEY (category_id) REFERENCES categories(category_id),
-    FOREIGN KEY (user_id, category_id, account_id, year_id, qtr_id) REFERENCES qtr_total(user_id, category_id, account_id, year_id, qtr_id),
-    PRIMARY KEY(user_id, category_id, account_id, month_id, qtr_id, year_id)
+    FOREIGN KEY (year_id, qtr_id, user_id, category_id) REFERENCES qtr_total(year_id, qtr_id, user_id, category_id),
+    PRIMARY KEY(year_id, qtr_id, month_id, user_id, category_id)
 );
 
--- Weeks 1-52
--- insert parent qtr_total if not found
-DROP TABLE IF EXISTS week_total;
-CREATE TABLE week_total (
-    week_id TINYINT(1) NOT NULL,	
-	month_id TINYINT(1) NOT NULL, -- formula(month_from_week)
-    qtr_id TINYINT(1) NOT NULL, -- formula(qtr_from_month)
-	year_id INT NOT NULL DEFAULT 2023, -- formula(year_from?)
-	user_id INT NOT NULL,
-    account_id INT NOT NULL,
-    category_id INT NOT NULL,
-    budget_total  DECIMAL(10, 2) NOT NULL, -- SUM(budet.amount)
-    actual_amount DECIMAL(10, 2) NOT NULL, -- sum(transactions.amount)
-    difference DECIMAL(10, 2) NOT NULL, -- formula(budget - actual)
-    created_date  DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id),
-    FOREIGN KEY (category_id) REFERENCES categories(category_id),
-    FOREIGN KEY (user_id, category_id, account_id, month_id, qtr_id, year_id) REFERENCES month_total(user_id, category_id, account_id, month_id, qtr_id, year_id),
-    PRIMARY KEY(user_id, category_id, account_id, week_id,  month_id, qtr_id, year_id)
-);
 
 -- Create the 'budget' table to track weekly estimated vs actual data
 -- insert parent week_total if not found
 DROP TABLE IF EXISTS budget;
 CREATE TABLE budget (
-    budget_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    account_id INT NOT NULL,
-    category_id INT NOT NULL,
-    week_date  DATETIME DEFAULT CURRENT_TIMESTAMP,
-    month_id TINYINT(1) NOT NULL, -- formula(month_from_week)
-    qtr_id TINYINT(1) NOT NULL, -- formula(qtr_from_month)
-    week_id TINYINT(1) DEFAULT 1, -- formula(lookup week by week_date)
-    year_id INT DEfAULT 2023, -- formula(year_from_week_date)
+    budget_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    year_id INTEGER DEfAULT 2023, -- formula(year_from_week_date)
+    qtr_id INTEGER  NOT NULL, -- formula(qtr_from_month)
+    month_id INTEGER  NOT NULL, -- formula(month_from_week)
+    user_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
     description VARCHAR(200),
     amount DECIMAL(10, 2) NOT NULL, 
-    is_expense TINYINT(1) NOT NULL,  -- copy(category.is_expense)
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id),
+    budget_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    is_expense INTEGER ,  -- copy(category.is_expense)
+    FOREIGN KEY (user_id) REFERENCES tenant_user(user_id),
     FOREIGN KEY (category_id) REFERENCES categories(category_id),
-    FOREIGN KEY (user_id, category_id, account_id, week_id,  month_id, qtr_id, year_id) REFERENCES week_total(user_id, category_id, account_id, week_id,  month_id, qtr_id, year_id)
+    FOREIGN KEY (year_id, qtr_id, month_id, user_id, category_id) REFERENCES month_total(year_id, qtr_id, month_id, user_id, category_id)
 );
 
 -- Create the 'transactions' table to record financial transactions
-DROP TABLE IF EXISTS transaction;
+DROP TABLE IF EXISTS transactions;
 CREATE TABLE transactions (
-    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    account_id INT NOT NULL,
+    transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    year_id INTEGER DEfAULT 2023, -- formula(year_from_week_date)
+    month_id INTEGER  NOT NULL, -- formula(month_from_week)
+    qtr_id INTEGER  NOT NULL, -- formula(qtr_from_month)
+    user_id INTEGER NOT NULL,
     category_id INT,
+    account_id INTEGER NOT NULL,
     transaction_date  DATETIME DEFAULT CURRENT_TIMESTAMP,
-    year_id INT DEfAULT 2023, -- formula(year_from_week_date)
-    month_id TINYINT(1) NOT NULL, -- formula(month_from_week)
-    qtr_id TINYINT(1) NOT NULL, -- formula(qtr_from_month)
-    week_id TINYINT(1) DEFAULT 1, -- formula(lookup week by week_date)
     description VARCHAR(200),
     amount DECIMAL(10, 2) NOT NULL,
-    is_expense TINYINT(1) NOT NULL,  -- copy(category.is_expense)
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    is_expense INTEGER ,  -- copy(category.is_expense)
+    FOREIGN KEY (user_id) REFERENCES tenant_user(user_id),
     FOREIGN KEY (account_id) REFERENCES accounts(account_id),
     FOREIGN KEY (category_id) REFERENCES categories(category_id),
-    FOREIGN KEY (user_id, category_id, account_id, week_id,  month_id, qtr_id, year_id) REFERENCES week_total(user_id, category_id, account_id, week_id,  month_id, qtr_id, year_id)
+    FOREIGN KEY (year_id, qtr_id, month_id, user_id, category_id) REFERENCES month_total(year_id, qtr_id, month_id, user_id, category_id)
 );
