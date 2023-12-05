a7323c83840da809bdbd36a51eef180a5c4fc62c
Val Huber <valjhuber@gmail.com>
2023-12-04 19:17:10 -0800

    v# bump

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "09.05.28"
+__version__ = "09.05.29"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t12/03/2023 - 09.05.28: Thick, Add order: map & lookup, No sql logging in rules, nw msgs, curl post \n"\
+    "\t12/04/2023 - 09.05.29: Thick, Add order int: map & lookup, No sql logging in rules, nw msgs, curl post \n"\
     "\t11/19/2023 - 09.05.14: Run Config: Create servers/ApiLogicProject (new IDE) -- nw+, curl, curr-proj \n"\
     "\t11/12/2023 - 09.05.08: multi-db bug fix (24) \n"\
     "\t11/07/2023 - 09.05.07: basic_demo: scripted customizations, iteration \n"\
