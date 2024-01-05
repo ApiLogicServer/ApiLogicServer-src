aaeef20539f42b66321c6b6d20c66f0c4de68571
Val Huber <valjhuber@gmail.com>
2024-01-04 16:11:37 -0800

    10.01.01: Quoted col names, Default Interpreter for VS Code, allocation fix

--- api_logic_server_cli/prototypes/allocation/logic/declare_logic.py
+++ api_logic_server_cli/prototypes/allocation/logic/declare_logic.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 from logic_bank.exec_row_logic.logic_row import LogicRow
 from logic_bank.extensions.rule_extensions import RuleExtension
 from logic_bank.logic_bank import Rule
+import api.system.opt_locking.opt_locking as opt_locking
+from security.system.authorization import Grant
 from database import models
 import logging
 
@@ -32,3 +34,31 @@ def declare_logic():
     Allocate(provider=models.Payment,  # uses default (overrideable) while_calling_allocator
             recipients=unpaid_orders,
             creating_allocation=models.PaymentAllocation)
+    
+
+    ####
+    # Standard Rules Follow
+    ###
+
+    def handle_all(logic_row: LogicRow):  # OPTIMISTIC LOCKING, [TIME / DATE STAMPING]
+        """
+        This is generic - executed for all classes.
+
+        Invokes optimistic locking.
+
+        You can optionally do time and date stamping here, as shown below.
+
+        Args:
+            logic_row (LogicRow): from LogicBank - old/new row, state
+        """
+        if logic_row.is_updated() and logic_row.old_row is not None and logic_row.nest_level == 0:
+            opt_locking.opt_lock_patch(logic_row=logic_row)
+        enable_creation_stamping = True  # CreatedOn time stamping
+        if enable_creation_stamping:
+            row = logic_row.row
+            if logic_row.ins_upd_dlt == "ins" and hasattr(row, "CreatedOn"):
+                row.CreatedOn = datetime.datetime.now()
+                logic_row.log("early_row_event_all_classes - handle_all sets 'Created_on"'')
+            Grant.process_updates(logic_row=logic_row)
+
+    Rule.early_row_event_all_classes(early_row_event_all_classes=handle_all)
--- api_logic_server_cli/prototypes/allocation/test/test.sh
+++ api_logic_server_cli/prototypes/allocation/test/test.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+echo "\n\nPosting \$100 Payment to ALFKI\n"
+
 curl -X POST "http://localhost:5656/api/Payment/" -H  "accept: application/vnd.api+json" -H  "Content-Type: application/json" -d "{  \"data\": {    \"attributes\": {      \"Amount\": 100,      \"CustomerId\": \"ALFKI\"    },    \"type\": \"Payment\"  }}" > results.txt
 
 if grep -q '"AmountUnAllocated": 0.0' results.txt
     then
-        echo "pass -- AmountUnAllocated:0.0"
+        echo "\npass -- AmountUnAllocated:0.0\n\n"
     else
         if grep -q '"AmountUnAllocated":0.0' results.txt
             then
-                echo "pass -- AmountUnAllocated:0.0"
+                echo "\npass -- AmountUnAllocated:0.0\n\n"
             else
-                echo "fail -"
+                echo "\nfails -\n"
         fi
 fi
