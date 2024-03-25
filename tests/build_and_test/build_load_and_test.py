c43c6e29233de7edd4057390acd1d334d606e6d3
Val Huber <valjhuber@gmail.com>
2024-03-24 18:23:16 -0700

    wip 3.12

--- tests/build_and_test/build_load_and_test.py
+++ tests/build_and_test/build_load_and_test.py
@@ -797,7 +797,7 @@ if Config.do_install_api_logic_server:  # verify the build process - rebuild, an
             msg=f'\nInstall testpy logicbank')
 
     pyodbc = 'pyodbc==4.0.34'   # pre python 3.12
-    # pyodbc = 'psycopg2-binary'  # python 3.12 upgrade pyodbc==4.0.34 --> pyodbc==5.0.0
+    pyodbc = 'pyodbc==5.1.0'     # python 3.12 upgrade pyodbc==4.0.34 --> pyodbc==5.0.1
     if platform in["win32", "linux"]:  # val: FIXME
         print("mac only")
     else:  # upgrade pyodbc==4.0.34 --> pyodbc==5.0.0
