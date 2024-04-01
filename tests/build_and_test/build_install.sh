a74e7cad0b5d5158c5fa0ecb8cf4527cd67c03a6
Val Huber <valjhuber@gmail.com>
2024-03-31 17:13:31 -0700

    clean buids, build_and_install, add clean

--- tests/build_and_test/build_install.sh
+++ tests/build_and_test/build_install.sh
@@ -13,10 +13,18 @@ SCRIPT_DIR="$(pwd)"
 
 echo "..Script directory: $SCRIPT_DIR"
 
-cd $SCRIPT_DIR/../../../../build_and_test/ApiLogicServer
+pushd $SCRIPT_DIR/../../../../build_and_test/ApiLogicServer
 INSTALL_DIR=$(pwd)
+rm -r $INSTALL_DIR/*
+popd
 
-echo "..Installing to directory: $INSTALL_DIR"
+mkdir -p $SCRIPT_DIR/../../../../clean/ApiLogicServer
+pushd $SCRIPT_DIR/../../../../clean/ApiLogicServer
+CLEAN_DIR=$(pwd)
+rm -r $CLEAN_DIR/*
+popd
+
+echo "..Installing to directories: \n....$INSTALL_DIR\n....$CLEAN_DIR\n"
 
 cd $SCRIPT_DIR
 cd ../..
@@ -28,17 +36,24 @@ PY=$1
 
 echo "..Using Python: $PY\n"
 
+rm -rf $SRC_DIR/build
+rm -rf $SRC_DIR/dist
+rm -rf $SRC_DIR/ApiLogicServer.egg-info
+
 # exit 1
 
 cd $SRC_DIR
 $PY -m build
 
+
+###################################################
+# Install into build_and_test/ApiLogicServer
+###################################################
+
 cd $INSTALL_DIR
 python3 -m venv venv
 
 . venv/bin/activate
-
-# exit 1
 
 # python3 -m pip install -r requirements.txt
 
@@ -47,6 +62,26 @@ set -x
 # python3 -m pip install $SRC_DIR
 $PY -m pip install $SRC_DIR
 set +x
+
+
+
+###################################################
+# Install into clean/ApiLogicServer
+###################################################
+
+cd $CLEAN_DIR
+python3 -m venv venv
+
+. venv/bin/activate
+
+# python3 -m pip install -r requirements.txt
+
+# ls $CLEAN_DIR/../../..
+set -x
+# python3 -m pip install $SRC_DIR
+$PY -m pip install $SRC_DIR
+set +x
+
 
 echo "Complete.\n\n"
 exit 0
