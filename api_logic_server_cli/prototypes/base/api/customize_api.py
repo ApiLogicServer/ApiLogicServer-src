9a3725e8441b9dab53032f07f184f0bc2fad774f
Val Huber <valjhuber@gmail.com>
2024-05-12 08:03:16 -0700

    discovery - logic & apis

--- api_logic_server_cli/prototypes/base/api/customize_api.py
+++ api_logic_server_cli/prototypes/base/api/customize_api.py
@@ -21,7 +21,7 @@ def expose_services(app, api, project_dir, swagger_host: str, PORT: str):
     
     app_logger.debug("api/customize_api.py - expose custom services")
 
-    from api.services.auto_discovery import discover_services
+    from api.api_discovery.auto_discovery import discover_services
     discover_services(app, api, project_dir, swagger_host, PORT)
 
     @app.route('/hello_world')
--- api_logic_server_cli/prototypes/nw/api/customize_api.py
+++ api_logic_server_cli/prototypes/nw/api/customize_api.py
@@ -66,7 +66,7 @@ def expose_services(app, api, project_dir, swagger_host: str, PORT: str):
 
     app_logger.debug("api/customize_api.py - expose custom services")
 
-    from api.services.auto_discovery import discover_services
+    from api.api_discovery.auto_discovery import discover_services
     discover_services(app, api, project_dir, swagger_host, PORT)
 
     api.expose_object(ServicesEndPoint)  # Swagger-visible services
