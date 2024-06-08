6ee874d2bbe4d6dab8494ceba807082f970e592f
Val Huber <valjhuber@gmail.com>
2024-06-08 15:59:29 -0700

    basic demo += kafka (as microservice)

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "10.04.53"
+__version__ = "10.04.54"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t06/08/2024 - 10.04.53: default-auth creation, BLT runs, basic_demo? \n"\
+    "\t06/08/2024 - 10.04.54: default-auth creation, BLT runs, basic_demo+? \n"\
     "\t06/07/2024 - 10.04.49: fix config-driven security, failing Werkzeug>=2.3.8 \n"\
     "\t06/06/2024 - 10.04.48: config-driven security config for admin.yaml \n"\
     "\t06/04/2024 - 10.04.47: ont cascade add, mgr: fix missing env, docker mgr, BLT behave logs, add-cust \n"\
