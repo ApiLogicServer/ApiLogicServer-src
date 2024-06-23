9264de584d7e9029f579c3039b7cd455088961cd
Val Huber <valjhuber@gmail.com>
2024-06-23 10:55:15 -0700

    pre_post.prompt fixes

--- api_logic_server_cli/cli.py
+++ api_logic_server_cli/cli.py
@@ -551,7 +551,7 @@ def tutorial(ctx, create):
               help="SQLAlchemy Database URL\n")
 @click.option('--version', 'genai_version',
               default='gpt-3.5-turbo',
-              help="Use AI version#")
+              help="Eg, gpt-3.5-turbo, gpt-4o")
 @click.option('--gen-using-file', 'gen_using_file',
               default='',
               help="Use ChatGPT API")
