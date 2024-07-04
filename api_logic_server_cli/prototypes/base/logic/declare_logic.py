5eb407a5e214f519ba9ee3d5623c3d918add0ade
Val Huber <valjhuber@gmail.com>
2024-07-04 07:51:41 -0700

    10.04.88: Public role support for no-role users, user p1 in sql & dc auth, genai docs, user auditing

--- api_logic_server_cli/prototypes/base/logic/declare_logic.py
+++ api_logic_server_cli/prototypes/base/logic/declare_logic.py
@@ -5,7 +5,7 @@ from logic_bank.extensions.rule_extensions import RuleExtension
 from logic_bank.logic_bank import Rule
 from database import models
 import api.system.opt_locking.opt_locking as opt_locking
-from security.system.authorization import Grant
+from security.system.authorization import Grant, Security
 import logging
 
 app_logger = logging.getLogger(__name__)
@@ -23,27 +23,39 @@ def declare_logic():
     from logic.logic_discovery.auto_discovery import discover_logic
     discover_logic()
 
-    def handle_all(logic_row: LogicRow):  # OPTIMISTIC LOCKING, [TIME / DATE STAMPING]
+    def handle_all(logic_row: LogicRow):  # #als: TIME / DATE STAMPING, OPTIMISTIC LOCKING
         """
         This is generic - executed for all classes.
 
-        Invokes optimistic locking.
+        Invokes optimistic locking, and checks Grant permissions.
 
-        You can optionally do time and date stamping here, as shown below.
+        Also provides user/date stamping.
 
         Args:
             logic_row (LogicRow): from LogicBank - old/new row, state
         """
         if logic_row.is_updated() and logic_row.old_row is not None and logic_row.nest_level == 0:
             opt_locking.opt_lock_patch(logic_row=logic_row)
-        enable_creation_stamping = False  # CreatedOn time stamping
-        if enable_creation_stamping:
+
+        Grant.process_updates(logic_row=logic_row)
+
+        did_stamping = False
+        if enable_stamping := False:  # #als:  DATE / USER STAMPING
             row = logic_row.row
             if logic_row.ins_upd_dlt == "ins" and hasattr(row, "CreatedOn"):
                 row.CreatedOn = datetime.datetime.now()
-                logic_row.log("early_row_event_all_classes - handle_all sets 'Created_on"'')
-            Grant.process_updates(logic_row=logic_row)
-
+                did_stamping = True
+            if logic_row.ins_upd_dlt == "ins" and hasattr(row, "CreatedBy"):
+                row.CreatedBy = Security.current_user().id  # TODO:  what if not enabled?
+                did_stamping = True
+            if logic_row.ins_upd_dlt == "upd" and hasattr(row, "UpdatedOn"):
+                row.UpdatedOn = datetime.datetime.now()
+                did_stamping = True
+            if logic_row.ins_upd_dlt == "upd" and hasattr(row, "UpdatedBy"):
+                row.UpdatedBy = Security.current_user().id  # TODO:  what if not enabled?
+                did_stamping = True
+            if did_stamping:
+                logic_row.log("early_row_event_all_classes - handle_all did stamping")     
     Rule.early_row_event_all_classes(early_row_event_all_classes=handle_all)
 
     #als rules report
