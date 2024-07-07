7ae4a721001d44cb61ca75faa94b9c85b8973ff6
Tyler Band <tylerm007@gmail.com>
2024-07-07 11:38:41 -0400

    add logon to ont home page

--- api_logic_server_cli/prototypes/ont_app/ontimize_seed/src/app/main/main.component.ts
+++ api_logic_server_cli/prototypes/ont_app/ontimize_seed/src/app/main/main.component.ts
@@ -1,9 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainComponent {
+  public closedSidenavImage: string;
+  public openedSidenavImage: string;
+  constructor(protected router: Router) { }
+
+  openAbout() {
+    this.router.navigate(["/main/about"]);
+  }
+
 }
+
