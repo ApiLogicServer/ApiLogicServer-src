9e39c85d9ed0709aad63050782570824d902b7f2
Val Huber <valjhuber@gmail.com>
2024-07-14 12:12:19 -0700

    fix auth models location for devops docker compose local

--- api_logic_server_cli/prototypes/base/devops/docker-compose-dev-local/docker-compose.sh
+++ api_logic_server_cli/prototypes/base/devops/docker-compose-dev-local/docker-compose.sh
@@ -18,7 +18,7 @@ fi
 
 pwd
 
-if [ ! -f "./../../database/authentication_models.py" ] 
+if [ ! -f "./../../database/database_discovery/authentication_models.py" ] 
 then
     echo "\nYou need to activate security first.  With mysql-container running...\n" 
     echo "ApiLogicServer add-auth --project_name=. --db_url=mysql+pymysql://root:p@localhost:3306/authdb"
