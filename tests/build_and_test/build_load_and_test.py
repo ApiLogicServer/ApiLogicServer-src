5e37562324f5422a5272bdc3e63e309bd1da25d9
Val Huber <valjhuber@gmail.com>
2024-07-23 08:06:53 -0700

    fix auth_admin - api endpoint removed

--- tests/build_and_test/build_load_and_test.py
+++ tests/build_and_test/build_load_and_test.py
@@ -779,8 +779,11 @@ if Config.do_install_api_logic_server:  # verify the build process - rebuild, an
 
     if venv_with_python:  # windows only (sigh... never found way to set venv with Python on Ubuntu)
         api_logic_server_home_path = api_logic_server_tests_path.parent
+        # install_api_logic_server_path: C:/Users/val/dev/ApiLogicServer/ApiLogicServer-dev/build_and_test/ApiLogicServer
+        assert install_api_logic_server_path.exists(), f"Win build error - bad blt path {install_api_logic_server_path}"
         build_cmd = f'{python} setup.py sdist bdist_wheel'
-        build_cmd = 'python -m build'
+        # python -m build --outdir=C:\Users\val\dev\ApiLogicServer\ApiLogicServer-dev\build_and_test\ApiLogicServer
+        build_cmd = f'{python} -m build --outdir={install_api_logic_server_path}'
         result_build = run_command(build_cmd,
             cwd=api_logic_server_home_path,
             msg=f'\nBuild ApiLogicServer at: {str(api_logic_server_home_path)}')
