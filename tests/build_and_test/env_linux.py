618fbc9ab2ed11cb4c2f8d5f33fd30418a9c3d8b
Val Huber <valjhuber@gmail.com>
2024-07-28 13:27:43 -0700

    no pg auth for linus

--- tests/build_and_test/env_linux.py
+++ tests/build_and_test/env_linux.py
@@ -42,7 +42,7 @@ class Config:
 
     do_docker_mysql = default_setting               # requires docker database be running
     do_docker_postgres = default_setting            # requires docker database be running
-    do_docker_postgres_auth = default_setting       # requires docker database be running
+    do_docker_postgres_auth = False       # requires docker database be running
     do_docker_sqlserver = False           # requires docker database be running
 
     do_docker_creation_tests = False      # build docker image, start it and create projects
