216d34eab57b66714132b65b638d2d0612869a51
Val Huber <valjhuber@gmail.com>
2024-07-29 16:15:59 -0700

    v#

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "11.00.03"
+__version__ = "11.00.04"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t07/28/2024 - 11.00.03: App Model Editor Re-Design, Ont bugs, Internal win blt \n"\
+    "\t07/28/2024 - 11.00.04: App Model Editor Re-Design, Ont kc bugs, Internal win blt \n"\
     "\t07/25/2024 - 11.00.00: Keycloak, App Model Editor \n"\
     "\t06/06/2024 - 10.04.48: config-driven admin.yaml security config \n"\
     "\t06/04/2024 - 10.04.47: ont cascade add, mgr: fix missing env, docker mgr, BLT behave logs, add-cust \n"\
