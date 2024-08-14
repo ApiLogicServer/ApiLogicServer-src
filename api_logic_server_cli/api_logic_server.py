791fbf9f898a3ef58151c735358296cb63aa905e
Val Huber <valjhuber@gmail.com>
2024-08-14 16:37:11 -0700

    11.00.24: DND handle classes table (reserved word), TB fix (?)

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,12 +12,12 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "11.00.23"
+__version__ = "11.00.24"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
+    "\t08/14/2024 - 11.00.24: DND handle classes table (reserved word), TB fix (?)  \n"\
     "\t08/13/2024 - 11.00.23: ont auth update, AME model changes.  BLT passes  \n"\
     "\t08/12/2024 - 11.00.22: dflt gpt4, save responses for each try, ignore .sh code, decimal fixes  \n"\
-    "\t08/09/2024 - 11.00.21: Same as 11.00.20, to test Web/GenAI release process \n"\
     "\t08/09/2024 - 11.00.20: GenAI hardening, diagnostics \n"\
     "\t08/02/2024 - 11.00.07: App Model Editor UI rework, docs and fix for kc and export \n"\
     "\t07/25/2024 - 11.00.00: Keycloak, App Model Editor \n"\
