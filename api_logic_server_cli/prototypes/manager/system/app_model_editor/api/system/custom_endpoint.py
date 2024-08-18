2fe3c50ce20fbe7ff843cde5f39587d71b502b81
Tyler Band <tylerm007@gmail.com>
2024-08-18 12:09:05 -0400

    upgrade AME to latest base api expression parser

--- api_logic_server_cli/prototypes/manager/system/app_model_editor/api/system/custom_endpoint.py
+++ api_logic_server_cli/prototypes/manager/system/app_model_editor/api/system/custom_endpoint.py
@@ -27,7 +27,10 @@ resource_logger = logging.getLogger("api.customize_api")
 
 db = safrs.DB 
 """this is a safrs db not DB"""
+
 session = db.session  # type: sqlalchemy.orm.scoping.scoped_session
+
+
 class DotDict(dict):
     """ dot.notation access to dictionary attributes """
     # thanks: https://stackoverflow.com/questions/2352181/how-to-use-a-dot-to-access-members-of-dictionary/28463329
@@ -139,6 +142,7 @@ class CustomEndpoint():
         from api.api_discovery.ontimize_api import getMetaData
         resources = getMetaData(key)
         self._attributes = resources["resources"][key]["attributes"]
+        self._quote = '`' if Args.backtic_as_quote else '"'
         
     def __str__(self):
             return  f"Alias {self.alias} Model: {self._model_class.__name__} PrimaryKey: {self.primaryKey} FilterBy: {self.filter_by} OrderBy: {self.order_by}"
@@ -216,6 +220,7 @@ class CustomEndpoint():
         Returns:
             dict: data dict from sql
         """
+        expressions = []
         args = {}
         payload = {}
         result = {}
@@ -227,7 +232,8 @@ class CustomEndpoint():
             method = request.method
             self._method = method
             args = request.args
-            payload = json.loads(request.data.decode('utf-8'))
+            if len(request.data) > 0:
+                payload = json.loads(request.data.decode('utf-8'))
             self._printIncludes(1) # debug print
             if method == 'DELETE':
                 raise ValidationError( 'Delete is not supported at this time')
@@ -254,11 +260,12 @@ class CustomEndpoint():
         resource_logger.debug(f"CustomEndpoint execute on: {self._model_class_name} using alias: {self.alias}")
         filter_by = None
         #key = args.get(pkey) if args.get(pkey) is not None else args.get(f"filter[{pkey}]")
+        _quote = '`' if Args.backtic_as_quote else '"'
         if value is not None and value != 'undefined':
-            filter_by = f'"{pkey}" = {self.quoteStr(value)}'
+            filter_by = f'{_quote}{pkey}{_quote} = {self.quoteStr(value)}'
             self._pkeyList.append(self.quoteStr(value))
         elif altKey is not None:
-            filter_by = f'"{pkey}" = {self.quoteStr(altKey)}'
+            filter_by = f'{_quote}{pkey}{_quote} = {self.quoteStr(altKey)}'
             self._pkeyList.append(self.quoteStr(altKey))
         filter_by = filter_by if filter_ is None else f"{filter_by} and {filter_}" if filter_by is not None else filter_
         self._href = f"{request.url_root[:-1]}{request.path}"
@@ -268,7 +275,7 @@ class CustomEndpoint():
             self._createRows(limit=limit,offset=offset,order_by=order_by,filter_by=filter_by, expressions=expressions) 
             self._executeChildren()
             self._modifyRows(result)
-            return json.dumps(result)
+            return json.dumps(result, indent=4, ensure_ascii=False).encode('utf8')
         except Exception as ex:
             resource_logger.error(f"CustomEndpoint error {ex}")
             return f"'error': {ex}"
@@ -709,8 +716,8 @@ class CustomEndpoint():
                 response = requests.post(url=url, json=j) 
             elif method in ["PUT","PATCH" ]:
                 response = requests.patch(url=f"{url}/{key}", data=j) 
-                
-        return json.dumps(json.loads(response.text)["data"]["attributes"]) if response.status_code < 301 else response.content
+        data  = json.loads(response.text)["data"]["attributes"]        
+        return json.dumps(data,indent=4, ensure_ascii=False).encode('utf8') if response.status_code < 301 else response.content
 
     def populateClass(self, clz, payload):
         for p in payload:
@@ -857,7 +864,8 @@ class CustomEndpoint():
         try:
             if self._method == 'OPTIONS':
                 return json_
-            json_dict = json.loads(json_) if isinstance(json_, str) else json_
+            #TODO - fixup asscii to utf-8
+            json_dict = json.loads(json_) if isinstance(json_, bytes) else json_
             json_result = json_dict.get(key, json_dict) if key in json_dict else json_dict if isinstance(json_dict, list) else [json_dict]
         except Exception as ex:
             resource_logger.error(f"Transform Error on style {style} using key: {key} on {json_} error: {ex}")
@@ -931,7 +939,7 @@ class CustomEndpoint():
         for each_child_def in custom_endpoint_child_list:
             child_property_name = each_child_def.role_name
             if child_property_name == '':
-                child_property_name = "OrderList"  # FIXME default from class name
+                child_property_name = "OrderList"  # TODO default from class name
             if child_property_name.startswith('Product'):
                 debug = 'good breakpoint'
             row_dict_child_list = getattr(row, child_property_name)
@@ -983,7 +991,7 @@ class CustomEndpoint():
             if child_property_name in row_dict:
                 row_dict_child_list = row_dict[child_property_name]
                 # row_as_dict[each_child_def.alias] = []  # set up row_dict child array
-                if each_child_def.isParent:  # FIXME not support (but TODO Lookup!)
+                if each_child_def.isParent:
                     #the_parent = getattr(row, child_property_name)
                     #the_parent_to_dict = self.to_dict(row = the_parent, current_endpoint = each_child_def)
                     #row_as_dict[each_child_def.alias].append(the_parent_to_dict)
