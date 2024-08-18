a94242c842cfe896c4a37549144633617a1cd46b
Thomas Pollet <thomas.pollet@gmail.com>
2024-08-18 16:26:09 +0200

    added gunicorn switch

--- api_logic_server_cli/prototypes/base/run.sh
+++ api_logic_server_cli/prototypes/base/run.sh
@@ -9,24 +9,23 @@ contains()
 
 
 debug() {
-  echo "$1"
+  echo -e "$1"
 }
 
 debug "\nAPI Logic Project Runner 1.0 Here ($# arg(s): $1)"
 if [ $# -eq 1 ]
   then
     if contains "help" $1; then
-      echo "\nRuns API Logic Project"
+      echo -e "\nRuns API Logic Project"
       echo "  sh run.sh [ calling | $ | help ]"
       echo "    no args - use project venv"
       echo "    calling - use venv from calling script (for internal tests)"
-      echo "    $ - use the current venv\n"
+      echo -e "    $ - use the current venv\n"
       exit 0
     elif [ "$1" == "$" ]; then
       debug ".. Using existing venv -- $VIRTUAL_ENV"
-    else
+    elif [ -d $PWD/venv ]; then
       debug ".. Calling directory - venv from $PWD"
-      cd $PWD
       . venv/bin/activate
     fi
   else
@@ -39,6 +38,13 @@ if [ $# -eq 1 ]
     fi
   fi
 
+export PYTHONHASHSEED=0
 cd "$(dirname "$0")"
-PYTHONHASHSEED=0
-python3 api_logic_server_run.py
+
+if contains gunicorn $1; then
+    shift
+    PORT=${APILOGICPROJECT_PORT:-5656}
+    gunicorn --log-level=info -b 0.0.0.0:${PORT} -w2 --reload api_logic_server_run:flask_app $*
+else
+    python api_logic_server_run.py $*
+fi
