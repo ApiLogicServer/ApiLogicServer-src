ac22adf1d14f468edb73d108b070054423257d2b
Val Huber <valjhuber@gmail.com>
2024-08-25 21:38:38 -0700

    11.01.04: safrs 3.1.4, genai: more decimal fixes

--- api_logic_server_cli/genai.py
+++ api_logic_server_cli/genai.py
@@ -288,7 +288,7 @@ class GenAI(object):
                     each_line = each_line.replace('end_time(datetime', 'end_time=datetime')
                 if indents_to_remove > 0:
                     each_line = each_line[indents_to_remove:]
-                use_relns = True
+                use_relns = False  # airport4 fails with ould not determine join condition between parent/child tables on relationship Airport.flights
                 if 'relationship(' in each_line and use_relns == False:
                     if each_line.startswith('    '):
                         each_line = each_line.replace('    ', '    # ')
