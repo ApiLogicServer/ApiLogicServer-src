38a3eec589829f45d388ad91142a8c83443ce737
Val Huber <valjhuber@gmail.com>
2024-09-02 08:48:08 -0700

    blt.sh

---
+++ tests/build_and_test/blt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+debug() {
+  echo "$1"
+}
+
+debug "\nAPI Logic Project Runner Build, Load and Test 1.0 Here ($# arg(s): $1 $2)"
+if [ $# -eq 0 ]
+  then
+    echo "\n  Usage:"
+    echo "    cd ApiLogicServer-src"
+    echo "    sh blt.sh [ s ]"
+    echo "\n"
+    exit 0
+  fi
+
+echo "\nRunner Build, Load and Test Running"
+echo "\n"
+
+python3 tests/build_and_test/build_load_and_test.py
+
+echo "\nRunner Build, Load and Test completed: $?\n"
