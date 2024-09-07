e676d6324d199f4fd7a0fc3b48d7ab61887a72ba
Val Huber <valjhuber@gmail.com>
2024-09-06 20:56:43 -0700

    11.01.17: Design for conversational creation using directory (not impl), optLock fix

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "11.01.16"
+__version__ = "11.01.17"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t09/05/2024 - 11.01.16: Design for conversational creation using directory (not impl) \n"\
+    "\t09/06/2024 - 11.01.17: Design for conversational creation using directory (not impl), optLock fix \n"\
     "\t09/04/2024 - 11.01.15: APILOGICPROJECT_LOG_CONFIG, _STOP_OK, _EXTERNAL_HOST, _EXTERNAL_PORT, genai descriptions \n"\
     "\t09/03/2024 - 11.01.11: logicbank 1.20.7, safrs 3.1.4, sra Aug17,  behave_run err-check[70], genai decimals \n"\
     "\t08/21/2024 - 11.01.02: Bug 68: nested quotes, internal (BLT path, exp tests) \n"\
