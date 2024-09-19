5ff97718c53e2e788e3df41afc2434531b784858
Tyler Band <tylerm007@gmail.com>
2024-09-19 10:51:36 -0400

    update Ont routing for JSONAPI

--- api_logic_server_cli/prototypes/ont_app/ontimize_seed/src/app/app-routing.module.ts
+++ api_logic_server_cli/prototypes/ont_app/ontimize_seed/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from 'ontimize-web-ngx';
 
 export const routes: Routes = [
-  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
+  {
+    path: 'main',
+    canActivate: [AuthGuardService],
+    loadChildren: () => import('./main/main.module').then(m => m.MainModule)
+  },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: '', redirectTo: 'main', pathMatch: 'full' }
 ];
