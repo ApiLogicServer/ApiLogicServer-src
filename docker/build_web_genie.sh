512195c8fd9e05c086b8ca190631620eccab7b2e
Val Huber <valjhuber@gmail.com>
2024-10-01 09:24:50 -0700

    got a bit futher

--- docker/build_web_genie.sh
+++ docker/build_web_genie.sh
@@ -17,6 +17,11 @@ fi
 #   echo $ostype contains ubuntu
 # fi
 
+# if you have installed apilogicserver_dev:
+# cd ~/dev/ApiLogicServer/ApiLogicServer-dev/org_git/ApiLogicServer-src
+# cd ..
+# sh docker/build_web_genie.sh clean
+
 # normally true, use false for skipping long clone during testing 
 clonedocs=true
 
@@ -31,26 +36,46 @@ if [ $# -eq 0 ]
     echo " "
     echo " Note: creates a folder at pwd: $webgen_ai_docker"
     echo " "
-    echo "   > sh build_web_genie.sh go"
+    echo "   > sh build_web_genie.sh [go | clean]"
     echo " "
     exit 0
   else
     ls
     echo " "
+    if [ "$1" = "clean" ]
+      then
+        echo "\nClean $webgen_ai_docker\n"
+        rm -rf $webgen_ai_docker
+    fi
     mkdir webgen_ai_docker
     cd webgen_ai_docker
     touch webgenai_env
+
+    mkdir nginx
+    cd nginx
+    curl -o webgenie.Dockerfile https://raw.githubusercontent.com/ApiLogicServer/ApiLogicServer-src/refs/heads/main/nginx/nginx.conf
+    curl -o webgenie.Dockerfile https://raw.githubusercontent.com/ApiLogicServer/ApiLogicServer-src/refs/heads/main/nginx/wg.conf
+    cd ..
+
     curl -o webgenie.Dockerfile https://raw.githubusercontent.com/ApiLogicServer/ApiLogicServer-src/refs/heads/main/docker/webgenie.Dockerfile
+
     set -x
     # Build wg:
     git clone https://github.com/ApiLogicServer/sra --depth=1
     git clone https://github.com/ApiLogicServer/webgenai --depth=1
 
-    cp -rf ../nginx .
+    # cp -rf ../nginx .
     docker build -f webgenie.Dockerfile -t apilogicserver/webgenie --no-cache  --rm .
 
     # Run wg (on port 8282):
     docker run -it --rm --name webgenie -p 8282:80  --env-file webgenai_env   apilogicserver/webgenie
     set +x
 fi
+
+
+# if you have installed apilogicserver_dev:
+# cd ~/dev/ApiLogicServer/ApiLogicServer-dev/org_git/ApiLogicServer-src
+# cd ..
+# sh apilogicserver-src/docker/build_web_genie.sh clean
+
 exit 0
