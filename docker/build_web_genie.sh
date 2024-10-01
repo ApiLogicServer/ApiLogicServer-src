9589894bbe092b9e9f1f4bd2d9e9c825b3b5d861
Val Huber <valjhuber@gmail.com>
2024-10-01 08:18:00 -0700

    wip - build_web_genie.sh

--- docker/build_web_genie.sh
+++ docker/build_web_genie.sh
@@ -18,98 +18,35 @@ fi
 # normally true, use false for skipping long clone during testing 
 clonedocs=true
 
+webgen_ai_docker='webgen_ai_docker'
+
 if [ $# -eq 0 ]
   then
     echo " "
     # echo "shell: $SHELL"
-    echo "Web/GenAI Docker on $ostype (version 7.0.15)\n"
+    echo "\n\n Build Web/GenAI Docker"
+    echo "  on $ostype (version 7.0.15)\n"
     echo " "
-    echo " IMPORTANT - creates a folder: web_gen_ai, then builds"
-    echo "   > mkdir ApiLogicServer"
+    echo " Note: creates a folder at pwd: $webgen_ai_docker"
     echo " "
-    echo "   > sh build_web_genie.sh"
+    echo "   > sh build_web_genie.sh go"
     echo " "
     exit 0
   else
     ls
     echo " "
-    mkdir web_gen_ai
-    curl -o filename raw-link-to-file
+    mkdir webgen_ai_docker
+    cd webgen_ai_docker
+    mkdir opt/webgenai_env
+    curl -o webgenie.Dockerfile https://github.com/ApiLogicServer/ApiLogicServer-src/blob/main/docker/webgenie.Dockerfile
     set -x
-    mkdir ApiLogicServer-dev
-    cd ApiLogicServer-dev
-    mkdir servers    # good place to create ApiLogicProjects
-    mkdir build_and_test
-    mkdir org_git  # git clones from org ApiLogicServer here
-    cd org_git
+    # Build wg:
+    git clone https://github.com/ApiLogicServer/sra
+    git clone https://github.com/ApiLogicServer/webgenai
+    docker build -f webgenie.Dockerfile -t apilogicserver/webgenie --no-cache  --rm .
 
-    if [ "$clonedocs" = true ]
-      then
-        git clone https://github.com/ApiLogicServer/Docs
-        cd Docs
-        python3 -m venv venv       # may require python -m venv venv
-        if contains "ubuntu" $ostype; then
-          echo $ostype contains ubuntu
-          . venv/bin/activate
-        else
-          echo $ostype does not contain ubuntu
-          source venv/bin/activate   # windows venv\Scripts\activate
-        fi
-        pip install -r requirements.txt
-        cd ..
-    fi
-
-    # pwd
-    # read -p "Ready to acquire - verify at ApiLogicServer-dev/org_git> "
-    # get sra runtime as ApiLogicServer-dev/build
-    curl https://github.com/thomaxxl/safrs-react-admin/releases/download/0.1.2/safrs-react-admin-0.1.2.zip -LO
-    echo "unzipping sra to build.."
+    # Run wg (on port 8282):
+    docker run -it --rm --name webgenie -p 8282:80  --env-file /opt/webgenai_env   apilogicserver/webgenie
     set +x
-    unzip safrs-react-admin-0.1.2.zip
-    set -x
-    
-    git clone https://github.com/ApiLogicServer/ApiLogicServer-src.git
-    cd ApiLogicServer-src
-    # pwd
-    # read -p "\nVerify at org_git/ApiLogicServer-src> "
-    echo "\ncopying build (sra - safrs-react-admin) --> ApiLogicServer"
-    cp -r ../build api_logic_server_cli/create_from_model/safrs-react-admin-npm-build
-
-    rm -r ../build
-    rm ../safrs-react-admin-0.1.2.zip
-    
-    #
-    #
-    # read -p "Installed - ready to launch IDE..."
-    if [ "$1" = "vscode" ]
-      then
-        python3 -m venv venv       # may require python -m venv venv
-        # pwd
-        # ls
-        . venv/bin/activate
-        # read -p "venv created; do optional pre-installs now $1> "
-        python3 -m pip install -r requirements.txt    # you may need to use pip3, or restart your terminal session
-        code .vscode/ApiLogicServerDev.code-workspace
-        set +x
-        echo ""
-        echo "Workspace opened; use pre-created Launch Configurations:"
-        echo "  * Run 1 - Create ApiLogicProject, then..."
-        echo "  * Run 2 - RUN ApiLogicProject"
-    elif [ "$1" = "charm" ]
-    then
-        charm .
-        set +x
-        echo "  * Python Interpreter > Add New Environment (default, to create venv)"
-        echo "     IMPORTANT - NOT DOCKER"
-        echo "  * then open requirements.txt - PyCharm should **Install Requirements**"
-        echo "     If this fails, use a terminal to run pip install -r requirements.txt"
-    else
-      set +x
-    fi
-    echo ""
-    echo "IDEs are preconfigured with run/launch commands to create and run the sample"
-    echo ""
-    echo "ApiLogicServer/react-admin contains shell burn-and-rebuild-react-admin"
-    echo ""
-    exit 0
 fi
+exit 0
