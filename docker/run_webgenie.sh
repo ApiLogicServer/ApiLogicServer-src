07c63038d0c88166bdc5857c0f760d8aa0e39758
Val Huber <valjhuber@gmail.com>
2024-10-04 08:02:54 -0700

    fix wrong wg image

--- docker/run_webgenie.sh
+++ docker/run_webgenie.sh
@@ -28,5 +28,8 @@ if [ $# -eq 0 ]
     echo " "
     exit 0
 
-docker run -it --rm --name webgenie -p 8282:80  --env-file docker/webgenie_docker/webgen_ai_docker/webgenai.env   apilogicserver/webgenie
+docker run -it --rm --name webgenie -p 8282:80  --env-file docker/webgenie_docker/webgen_ai_docker/webgenai.env   apilogicserver/web_genai
+
+# docker run -it --rm --name webgenie -p 8282:80  -v /Users/val/dev/ApiLogicServer/ApiLogicServer-dev/org_git/webgenai:/opt/webgenai --env-file /Users/val/dev/ApiLogicServer/ApiLogicServer-dev/org_git/ApiLogicServer-src/docker/webgenie_docker/webgen_ai_docker/webgenai.env   apilogicserver/web_genie
+
 exit 0
