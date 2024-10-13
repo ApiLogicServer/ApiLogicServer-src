9de2f024253305fc3c32dbc1c83214347f78ec91
Val Huber <valjhuber@gmail.com>
2024-10-12 20:31:41 -0700

    add auto conversation

--- tests/genai_tests/env.py
+++ tests/genai_tests/env.py
@@ -13,6 +13,7 @@ class Config:
 
     do_test_genai = default_setting                 # complex iteration
     do_test_iso = default_setting                   # complex iteration
+    do_test_auto_conv = default_setting             # ensure project rebuilt, not truncated
 
 
     # ***********************
