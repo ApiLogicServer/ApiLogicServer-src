3eeed4e88e4a915c422cf1ade707d2d5713d5ec6
Val Huber <valjhuber@gmail.com>
2024-10-12 20:52:30 -0700

    add auto conversation

--- tests/genai_tests/env_mac.py
+++ tests/genai_tests/env_mac.py
@@ -9,44 +9,11 @@ class Config:
     #   what tests to run
     # ***********************
 
-    default_setting = True  # simplify enable / disable most
+    default_setting = True  # simplify enable / disable most    
 
-    do_install_api_logic_server = default_setting   # verify build wheel and local 
-    do_logicbank_test = ""                          # use this testpy version (or '')
-    
-
-    do_create_api_logic_project = default_setting   # create the default project
-    do_run_api_logic_project = default_setting      # start the server 
-    do_test_api_logic_project = default_setting     # run the behave tests (test logic, api)
-    do_test_api_logic_project_with_auth = default_setting  # run the behave tests (test logic, api)
-    do_test_genai = default_setting                 # run the genai tests
-    do_test_multi_reln = default_setting            # run the genai tests on airport
-
-    do_create_shipping = default_setting            # run shipping to listen to kafka (might run manually)
-    do_run_shipping = False                         # run shipping to listen to kafka (might run manually)
-    do_run_nw_kafka = default_setting               # run default project, *with* kafka
-    do_test_nw_kafka = default_setting
-
-
-    do_rebuild_tests = default_setting              # rebuild from model, allembic
-
-    do_multi_database_test = default_setting        # add-db todo, add-auth
-
-    do_allocation_test = default_setting            # create / run / test allocation project
-
-    do_budget_app_test = default_setting            # insert_parent test
-
-    do_other_sqlite_databases = default_setting     # classic models
-
-    do_include_exclude = default_setting            # --db_url=table_filters_tests
-
-    do_docker_mysql = default_setting               # requires docker database be running
-    do_docker_postgres =  default_setting           # requires docker database be running
-    do_docker_postgres_auth = default_setting       # requires docker database be running
-    do_docker_sqlserver = default_setting           # requires docker database be running
-
-    do_docker_creation_tests = default_setting      # build docker image, start it and create projects
-
+    do_test_genai = default_setting                 # complex iteration
+    do_test_iso = default_setting                   # complex iteration
+    do_test_auto_conv = default_setting             # ensure project rebuilt, not truncated
 
 
     # ***********************
