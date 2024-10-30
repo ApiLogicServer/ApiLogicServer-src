b6975c440203e9ec6e45b7cba062d5c4d2407f0d
Val Huber <valjhuber@gmail.com>
2024-10-30 08:53:01 -0700

    local wb build from non-GA ALS

---
+++ docker/webgenie_docker/build_web_genie_local.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -e # exit on error
+
+contains()
+  # echo contains check $1 in $2
+  case "$1" in
+    (*"$2"*) true;;
+    (*) false;;
+  esac
+
+ostype=$(uname -a)
+if contains "Ubuntu" $ostype; then
+  ostype="ubuntu"
+fi
+# if contains "ubuntu" $ostype; then
+#   echo $ostype contains ubuntu
+# fi
+
+# Intende installed apilogicserver_dev:
+# cd ~/dev/ApiLogicServer/ApiLogicServer-dev/org_git/ApiLogicServer-src
+# cd ..
+# sh docker/webgenie_docker/build_web_genie.sh deploy
+
+webgen_ai_docker='webgen_ai_docker'
+
+SRC_DIR=$(pwd)
+
+echo "\n\nBuild Web/GenAI Docker locally (no push to docker hub)"
+
+if [ $# -eq 0 ]
+  then
+    echo " "
+    # echo "shell: $SHELL"
+    echo "  on $ostype (version 7.0.15)\n"
+    echo " "
+    echo " Note: creates a folder at pwd: $webgen_ai_docker"
+    echo " "
+    echo "   > cd ~/dev/ApiLogicServer/ApiLogicServer-dev/org_git/ApiLogicServer-src"
+    echo "   > sh docker/webgenie_docker/build_web_genie_local.sh local"
+    echo " "
+    exit 0
+fi
+
+echo " "
+# ls 
+echo "pwd: $(pwd)\n"
+echo "To build api_logic_server_local image:"
+echo ".. 1. BLT (with tomato), or"
+echo ".. 2. See 'Internal' at docker/api_logic_server.Dockerfile"
+read -p "Verify pwd = ApiLogicServer-src, local image ready (see above)> "
+echo " "
+
+cd ..
+ORG_GIT_DIR=$(pwd)
+echo "pwd: $(pwd)"
+
+echo " "
+echo "\nClean $webgen_ai_docker\n"
+set -x
+
+rm -rf $webgen_ai_docker
+cp -r $SRC_DIR/docker/webgenie_docker/$webgen_ai_docker ./
+cd $webgen_ai_docker
+touch webgenai_env
+
+# Build wg:
+git clone https://github.com/ApiLogicServer/sra --depth=1
+git clone https://github.com/ApiLogicServer/webgenai --depth=1
+
+if [ "$1" = "local" ]
+  then
+    docker build -f $SRC_DIR/docker/webgenie_docker/webgen_ai_docker/webgenie_local.Dockerfile -t apilogicserver/web_genai --no-cache  --rm .
+  fi
+
+set +x
+
+cd $SRC_DIR
+echo "\npwd: $(pwd)\n"
+echo "\nrun: docker run -it --rm --name webgenie -p 8282:80  --env-file ./../../webg-config/web_genai.txt  -v ./../../webg-projects:/opt/projects apilogicserver/web_genai \n "
+
+exit 0
---
+++ docker/webgenie_docker/webgen_ai_docker/webgenie_local.Dockerfile
@@ -0,0 +1,39 @@
+
+# see docker/webgenie_docker/build_web_genie_local.sh
+
+FROM apilogicserver/api_logic_server_local
+
+USER root
+
+RUN apt-get update
+RUN apt-get install -y nginx
+RUN curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && \
+    apt-get install -y nodejs && \
+    apt-get clean && rm -rf /var/lib/apt/lists/*
+
+RUN apt-get update && apt-get install wget -y \
+    && mkdir -p -m 755 /etc/apt/keyrings \
+    && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+    && chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && apt update \
+    && apt install gh -y
+
+RUN npm install -g @softwaretechnik/dbml-renderer
+COPY nginx/nginx.conf /etc/nginx/nginx.conf
+COPY nginx/wg.conf /etc/nginx/wg.conf
+RUN mkdir /etc/nginx/apis
+RUN chown -R api_logic_server /var/log/nginx /etc/nginx/apis
+RUN chown api_logic_server /var/lib/nginx
+RUN chmod 777 /run # TODO!! security issue?
+
+RUN mkdir -p /opt/projects
+COPY webgenai/webgenai /opt/webgenai
+RUN rm -fr /home/api_logic_server/api_logic_server_cli/create_from_model/safrs-react-admin-npm-build
+COPY sra/build /home/api_logic_server/api_logic_server_cli/create_from_model/safrs-react-admin-npm-build
+RUN chown -R api_logic_server /opt /home/api_logic_server
+USER api_logic_server
+
+EXPOSE 5656-7000
+
+CMD ["bash", "-c", "/opt/webgenai/arun.sh"]
