201665ebbc7bf70a598f4388d3a19c864527014c
Val Huber <valjhuber@gmail.com>
2024-10-31 07:31:31 -0700

    for current state

--- tests/build_and_test/env_linux.py
+++ tests/build_and_test/env_linux.py
@@ -20,12 +20,12 @@ class Config:
     do_test_api_logic_project = default_setting     # run the behave tests (test logic, api)
     do_test_api_logic_project_with_auth = default_setting  # run the behave tests (test logic, api)
     do_test_genai = default_setting                 # run the genai tests
-    do_test_multi_reln = default_setting            # run the genai tests on airport
+    do_test_multi_reln = False            # this suite broken per new parsed response format
 
     do_create_shipping = default_setting            # run shipping to listen to kafka (might run manually)
-    do_run_shipping = False               # run shipping to listen to kafka (might run manually)
-    do_run_nw_kafka = True               # run default project, *with* kafka
-    do_test_nw_kafka = True
+    do_run_shipping = False                         # run shipping to listen to kafka (might run manually)
+    do_run_nw_kafka = default_setting               # run default project, *with* kafka
+    do_test_nw_kafka = default_setting
 
 
     do_rebuild_tests = default_setting              # rebuild from model, allembic
@@ -40,12 +40,13 @@ class Config:
 
     do_include_exclude = default_setting            # --db_url=table_filters_tests
 
-    do_docker_mysql = False               # requires docker database be running
-    do_docker_postgres = False            # requires docker database be running
-    do_docker_postgres_auth = False       # requires docker database be running
+    do_docker_mysql = default_setting               # requires docker database be running
+    do_docker_postgres =  default_setting           # requires docker database be running
+    do_docker_postgres_auth = default_setting       # requires docker database be running
     do_docker_sqlserver = False           # requires docker database be running
 
-    do_docker_creation_tests = False      # build docker image, start it and create projects
+    do_docker_creation_tests = default_setting      # build docker image, start it and create projects
+
 
 
 
