851d42a17687b79ae1a45e5bf0d68f9fbe53042f
Val Huber <valjhuber@gmail.com>
2024-11-02 07:58:13 -0700

    minor import cleanup

--- api_logic_server_cli/prototypes/manager/system/genai/create_db_models_inserts/create_db_models_imports.py
+++ api_logic_server_cli/prototypes/manager/system/genai/create_db_models_inserts/create_db_models_imports.py
@@ -1,19 +1,20 @@
-# created from response - used to create database and project
-#  should run without error
-#  if not, check for decimal, indent, or import issues
+# created from response, to create create_db_models.sqlite, with test data
+#    that is used to create project
+# should run without error in manager 
+#    if not, check for decimal, indent, or import issues
 
 import decimal
 import logging
 import sqlalchemy
-from sqlalchemy.sql import func  # end imports from system/genai/create_db_models_inserts/create_db_models_prefix.py
+from sqlalchemy.sql import func 
 from logic_bank.logic_bank import Rule
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, Date, DateTime, Numeric, Boolean, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime
 from sqlalchemy.orm import relationship
-from datetime import date
+from datetime import date   
+from datetime import datetime
 
 logging.getLogger('sqlalchemy.engine.Engine').disabled = True  # remove for additional logging
 
-Base = declarative_base()
+Base = declarative_base()  # from system/genai/create_db_models_inserts/create_db_models_prefix.py
