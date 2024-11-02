ce4cd73ea3e3475d71d9001ec70954a8af5e1ac7
Val Huber <valjhuber@gmail.com>
2024-11-02 12:08:27 -0700

    changed temp loc

--- docker/webgenie_docker/build_web_genie_local.sh
+++ docker/webgenie_docker/build_web_genie_local.sh
@@ -76,7 +76,8 @@ set +x
 
 cd $SRC_DIR
 echo "\npwd: $(pwd)\n"
-echo "get wg temp: docker cp webgenie:/tmp ~/Desktop/wg-temp \n"
+echo "  (wg-temp formerly at: docker cp webgenie:/tmp ~/Desktop/wg-temp \n"
+echo "temp at ./../../webg-projects"
 echo "\nrun: docker run -it --rm --name webgenie -p 8282:80  --env-file ./../../webg-config/web_genai.txt  -v ./../../webg-projects:/opt/projects apilogicserver/web_genai \n "
 
 exit 0
