c8267ae702af1298c8952b0e4a7758f9fda0927a
Val Huber <valjhuber@gmail.com>
2024-11-02 16:04:33 -0700

    12.01.04: genai: retries fix,

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "12.01.03"
+__version__ = "12.01.04"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t11/02/2024 - 12.01.03: genai: catch 'datetime.datetime.utcnow' in classes \n"\
+    "\t11/02/2024 - 12.01.04: genai: retries fix, catch 'datetime.datetime.utcnow' in classes \n"\
     "\t11/01/2024 - 12.01.02: genai: default project name, cli arg tables (aka complexity) \n"\
     "\t10/31/2024 - 12.01.00: genai: informal rules (eg, Sum of employee salaries cannot exceed department budget) \n"\
     "\t10/21/2024 - 12.00.04: sra 10-22, Prelim support genai --using=dir/project \n"\
