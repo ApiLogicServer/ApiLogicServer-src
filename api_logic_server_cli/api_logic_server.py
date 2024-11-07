adfe0347c2e9085bb0ee41679a9ad502f69f437b
Val Huber <valjhuber@gmail.com>
2024-11-06 19:20:34 -0800

    12.01.17: genai: test data arg,

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "12.01.15"
+__version__ = "12.01.17"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t11/06/2024 - 12.01.16: genai: fix bogus Base, engine row names in test data code \n"\
+    "\t11/06/2024 - 12.01.17: genai: test data arg, fix bogus Base, engine row names in test data code, multi-rule indents \n"\
     "\t11/04/2024 - 12.01.14: genai: retries fix, ai-invented rules via disable, catch 'datetime.datetime.utcnow' in classes, tests \n"\
     "\t11/01/2024 - 12.01.02: genai: default project name, cli arg tables (aka complexity) \n"\
     "\t10/31/2024 - 12.01.00: genai: informal rules (eg, Sum of employee salaries cannot exceed department budget) \n"\
@@ -864,6 +864,7 @@ class ProjectRun(Project):
                      genai_prompt_inserts: str="",
                      genai_use_relns: bool=True,
                      genai_tables: int=0,
+                     genai_test_data_rows: int=0,
                      host: str='localhost', 
                      port: str='5656', 
                      swagger_host: str="localhost", 
@@ -908,6 +909,7 @@ class ProjectRun(Project):
         self.genai_use_relns = genai_use_relns
         """ Use relationships in create_db_models (internal) """
         self.genai_tables = genai_tables
+        self.genai_test_data_rows = genai_test_data_rows
         self.user_db_url = db_url  # retained for debug
         self.bind_key = bind_key
         self.api_name = api_name
