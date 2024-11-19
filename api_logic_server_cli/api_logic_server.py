32e7f820788e0d20c22c3c1ed1ea225f86c314f2
Val Huber <valjhuber@gmail.com>
2024-11-18 19:02:42 -0800

    12.02.00: genai: 'qualified any'

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,11 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "12.01.26"
+__version__ = "12.02.00"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t11/18/2024 - 12.01.26: genai: sqlite inserts, dataRows[], LogicBankOff (LBX), Readme Cleanup \n"\
-    "\t11/10/2024 - 12.01.22: genai: rule training, bug fix admin app Boolean fields, model via env \n"\
+    "\t11/18/2024 - 12.02.00: genai: 'qualified any' now supported in logic training \n"\
     "\t10/31/2024 - 12.01.00: genai: informal rules (eg, Sum of employee salaries cannot exceed department budget) \n"\
     "\t10/21/2024 - 12.00.04: sra 10-22, Prelim support genai --using=dir/project \n"\
     "\t10/12/2024 - 12.00.02: Natural Language Logic \n"\
