29685e2e876f61aea022cdcad3389611bca5465e
Val Huber <valjhuber@gmail.com>
2024-11-22 07:46:59 -0800

    ver

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "14.00.00"
+__version__ = "14.00.01"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t11/21/2024 - 14.00.00: genai: logic suggestions \n"\
+    "\t11/22/2024 - 14.00.01: genai: logic suggestions, bug fixes \n"\
     "\t11/18/2024 - 12.02.00: genai: 'qualified any' now supported in logic training \n"\
     "\t10/31/2024 - 12.01.00: genai: informal rules (eg, Sum of employee salaries cannot exceed department budget) \n"\
     "\t10/21/2024 - 12.00.04: sra 10-22, Prelim support genai --using=dir/project \n"\
