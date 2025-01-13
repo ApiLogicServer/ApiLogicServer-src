6c6787a9c9a15e7494d1afd43f4859cb07ec660e
Val Huber <valjhuber@gmail.com>
2025-01-13 05:47:03 -0800

    14.01.11: docs/logic_suggestions rework (passes manual test in manager readme)

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "14.01.10"
+__version__ = "14.01.11"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t01/12/2024 - 14.01.10: docs/logic_suggestions rework (passes manual test in manager readme) \n"\
+    "\t01/13/2024 - 14.01.11: docs/logic_suggestions rework (passes manual test in manager readme) \n"\
     "\t01/11/2024 - 14.01.09: import - WebG rules, with data, runs to completion \n"\
     "\t01/09/2024 - 14.01.05: test data from response per APILOGICPROJECT_NO_TEST_DATA, revised fixup tests \n"\
     "\t01/08/2024 - 14.01.04: import fixes, rebuild-test-data response fix, iterations rework, models2code fix no flask, prompt updates, call chapGPT sys.exit(1) \n"\
