623e8327bfa07df9e52c5fa77400f136fe52752e
Val Huber <valjhuber@gmail.com>
2025-01-19 21:04:36 -0800

    14.02.22: Issue 74: Multi-db support fixed, qualified models in rules (no import models.*)

--- api_logic_server_cli/prototypes/base/logic/declare_logic.py
+++ api_logic_server_cli/prototypes/base/logic/declare_logic.py
@@ -5,7 +5,7 @@ from logic_bank.extensions.rule_extensions import RuleExtension
 from logic_bank.logic_bank import Rule
 from logic_bank.logic_bank import DeclareRule
 import database.models as models
-from database.models import *
+# from database.models import *
 import api.system.opt_locking.opt_locking as opt_locking
 from security.system.authorization import Grant, Security
 import logging
