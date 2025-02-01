396883a92b50df19abcb7866fed07160c94b9620
Val Huber <valjhuber@gmail.com>
2025-01-31 17:43:53 -0800

    14.02.35: Issue 76: exp ending in ")",

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -15,7 +15,7 @@ Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 __version__ = "14.02.35"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t01/31/2024 - 14.02.35: Issue 76: exp ending in ")", Issue 74: Multi-db fix, extended default options, if-based Nat Lang formulas \n"\
+    "\t01/31/2024 - 14.02.35: Issue 76: exp ending in right paren, Issue 74: Multi-db fix, extended default options, if-based Nat Lang formulas \n"\
     "\t01/29/2024 - 14.02.34: save docs/response.json to fix bad rules for IS_GENAI_DEMO, LogicBank 1.20.23 (all defaults) \n"\
     "\t01/26/2024 - 14.02.31: genai_logic fail-safe, Issue 74: Multi-db fix, support models named Rule, wg logic discovery, if formula \n"\
     "\t01/17/2024 - 14.02.20: WebGenAI support, genai_demo \n"\
