87804816f1b0792721ba469264ac6a170e48f21e
Val Huber <valjhuber@gmail.com>
2025-03-07 08:04:26 -0800

    first attempt at comparing expected/actual hash - FAILED.1

--- docker/webgenie_docker/webgen_ai_docker/license/startup.sh
+++ docker/webgenie_docker/webgen_ai_docker/license/startup.sh
@@ -3,13 +3,14 @@
 # this script is run when the container starts
 # it checks the license and starts the WebGenAI service
 
+
 # for testing (not running), start the container with:
-# docker run -it --entrypoint /bin/bash apilogicserver/web_genai:latest
+# docker run -it --entrypoint /bin/bash -v /var/run/docker.sock:/var/run/docker.sock --user root apilogicserver/web_genai:latest
 # docker cp docker/webgenie_docker/webgen_ai_docker/license/startup.sh webgenie:/home/license/startup.sh
 
 echo " "
 
-echo "WebGenAI Startup 1.6"
+echo "WebGenAI Startup 1.8"
 echo " "
 
 # tamper protection - ensure container hash matches image tag saved in build
@@ -20,21 +21,16 @@ CONTAINER_ID=$(hostname)
 # Debug: Display the extracted container ID
 echo "Extracted container ID: $CONTAINER_ID"
 
-if [ -z "$CONTAINER_ID" ]; then
-    echo "Failed to retrieve container ID"
-    exit 1
-fi
-
 # Retrieve the container hash
 CONTAINER_HASH=$(docker inspect --format='{{.Id}}' $CONTAINER_ID)
+
+# Debug: Display the retrieved container hash
+echo "Retrieved container hash: $CONTAINER_HASH"
 
 if [ -z "$CONTAINER_HASH" ]; then
     echo "Failed to retrieve container hash"
     exit 1
 fi
-
-echo "Container hash: $CONTAINER_HASH"
-echo " "
 
 # Retrieve and display the tags of the image
 IMAGE_TAGS=$(docker images --format "{{.Tag}}" apilogicserver/web_genai)
@@ -43,8 +39,23 @@ echo "$IMAGE_TAGS"
 echo "End of tags"
 echo " "
 
+# Check if the container hash matches any of the tags
+if [[ "$IMAGE_TAGS" == *"$CONTAINER_HASH"* ]]; then
+    echo "Container hash matches one of the tags."
+else
+    echo "Container hash does not match any of the tags."
+    exit 1
+fi
+
 # Run the license checker
 python /home/license/license_checker.py
+
+
+# for testing, exit here
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 0
+fi
 
 # Check if the license checker was successful
 if [ $? -eq 0 ]; then
