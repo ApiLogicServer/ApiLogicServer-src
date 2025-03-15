edc81b807ac281f38484c188861d8d5f340fe2f4
Val Huber <valjhuber@gmail.com>
2025-03-14 18:13:33 -0700

    14.03.16: fix config security uri for wg docker run of nw+

--- api_logic_server_cli/prototypes/manager/run_web_genai.sh
+++ api_logic_server_cli/prototypes/manager/run_web_genai.sh
@@ -4,6 +4,6 @@
 # See: https://apilogicserver.github.io/Docs/WebGenAI-CLI/#configuration
 
 # cd <manager>
-# sh webgenai/run_web_genai.sh
+# sh run_web_genai.sh
 
 docker run -it --rm --name webgenai -p 8282:80  --env-file ./webgenai/webg_config/web_genai.txt -v ./webgenai/webg_temp:/tmp  -v ./webgenai/webg_config:/config -v ./webgenai/webg_projects:/opt/projects apilogicserver/web_genai
