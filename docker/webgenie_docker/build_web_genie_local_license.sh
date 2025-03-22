625406b4cade46397ca22eb99e540c6bc875420d
Val Huber <valjhuber@gmail.com>
2025-03-21 18:00:16 -0700

    local run note

--- docker/webgenie_docker/build_web_genie_local_license.sh
+++ docker/webgenie_docker/build_web_genie_local_license.sh
@@ -83,6 +83,7 @@ set +x
 # docker compose -f webgenai/docker-compose-webg.yml down
 
 # to start in interactive mode:
+# Add to docker-compose-webg.yml: command: /bin/bash
 # docker compose -f webgenai/docker-compose-webg.yml run web_genai
 # docker cp api_logic_server_utils.py webgenai://home/api_logic_server/api_logic_server_cli/create_from_model/api_logic_server_utils.py
 
