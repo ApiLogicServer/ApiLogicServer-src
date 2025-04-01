6c7e536b3d2279c457df0674557d123328ee87cb
Val Huber <valjhuber@gmail.com>
2025-04-01 12:48:37 -0700

    move graphics svcs to database

--- api_logic_server_cli/prototypes/base/api/expose_api_models.py
+++ api_logic_server_cli/prototypes/base/api/expose_api_models.py
@@ -49,13 +49,4 @@ def expose_models(api, method_decorators = []):
             app_logger.info(f"Exposing /{name}")
             api.expose_object(add_check_sum(obj), method_decorators= method_decorators)
 
-    try:  # sql auth provider - manage users/roles at: http:/localhost:5656/01/auth-admin/index.html#/Home
-        provider_name = Args.instance.security_provider.__dict__['__module__']
-        if Args.instance.security_enabled and 'security.authentication_provider.sql.auth_provider' == provider_name:
-            for name, obj in inspect.getmembers(database.database_discovery.authentication_models):
-                if inspect.isclass(obj) and issubclass(obj, database.models.SAFRSBaseX) and obj is not database.models.SAFRSBaseX:
-                    app_logger.info(f"Exposing /{name}")
-                    api.expose_object(add_check_sum(obj), method_decorators= method_decorators)
-    except Exception as e:
-        app_logger.warning(f"api/expose_api_models unable to expose sql authentication models: {e}")
     return api
