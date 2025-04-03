600df323519a0819aa11f34e9b30300cace8e48d
Val Huber <valjhuber@gmail.com>
2025-04-02 21:08:36 -0700

    14.03.29: db graphics svcs mult types

---
+++ api_logic_server_cli/prototypes/base/docs/training/logic_example.py
@@ -0,0 +1,41 @@
+import datetime
+from decimal import Decimal
+from logic_bank.exec_row_logic.logic_row import LogicRow
+from logic_bank.extensions.rule_extensions import RuleExtension
+from logic_bank.logic_bank import Rule
+from database import models
+import api.system.opt_locking.opt_locking as opt_locking
+from security.system.authorization import Grant
+import logging
+from flask import jsonify
+
+
+""" 
+logic should create python files in logic/logic_discovery, 
+and cross-check/use attribute names in database/models.py
+"""
+
+app_logger = logging.getLogger(__name__)
+
+
+def declare_logic():
+    """
+        Simple constraints for error testing 
+    """
+    Rule.constraint(validate=models.Customer,
+        as_condition=lambda row: row.CompanyName != 'x',
+        error_msg="CustomerName cannot be 'x'")
+
+    Rule.constraint(validate=models.Employee,
+        as_condition=lambda row: row.LastName != 'x',
+        error_msg="LastName cannot be 'x'")
+    
+    def valid_category_description(row: models.Category, old_row: models.Category, logic_row: LogicRow):
+        if logic_row.ins_upd_dlt == "upd":
+            return row.Description != 'x'
+        else:
+            return True
+    Rule.constraint(validate=models.Category,
+                    calling=valid_category_description,
+                    error_msg="Description cannot be 'x'")
+
