8a8ad84d9583c1c5ff0cfa71be6374ae03e4f565
Tyler Band <tylerm007@gmail.com>
2025-05-01 21:02:39 -0400

    update n8n to use getenv

--- api_logic_server_cli/prototypes/base/integration/n8n/n8n_producer.py
+++ api_logic_server_cli/prototypes/base/integration/n8n/n8n_producer.py
@@ -19,7 +19,18 @@ from integration.system.RowDictMapper import RowDictMapper
 from flask import jsonify
 import api.system.api_utils as api_utils
 from config.config import Args
+'''
+    wh_scheme = "http"
+    wh_server = "localhost" # or cloud.n8n.io...
+    wh_port = 5678
+    wh_endpoint = "webhook-test"
+    wh_path = "002fa0e8-f7aa-4e04-b4e3-e81aa29c6e69"
+    token = "YWRtaW46cA==" # Basic auth for n8n
+    N8N_PRODUCER = {"authorization": f"Basic {token}", "n8n_url": f'"{wh_scheme}://{wh_server}:{wh_port}/{wh_endpoint}/{wh_path}"'} 
+    # Or enter the n8n_url directly:
+    N8N_PRODUCER = {"authorization": f"Basic {token}","n8n_url":"http://localhost:5678/webhook-test/002fa0e8-f7aa-4e04-b4e3-e81aa29c6e69"}    
 
+'''
 producer = None
 """ connected producer (or null if N8N not enabled in Config.py) """
 
@@ -31,27 +42,57 @@ logger.debug("n8n_connect imported")
 
 def n8n_producer():
     """
-    Called by api_logic_server_run>server_setup to listen on kafka using confluent_kafka
 
-    Enabled by config.KAFKA_CONNECT (dict, of bootstrap.servers, client.id)
-
+    Enabled by config (dict, of bootstrap.servers, client.id)
+    OR
+    token = getenv("N8N_TOKEN", Args.instance.wh_token) #basic auth
+    scheme = getenv("N8N_SCHEME",Args.instance.wh_scheme) # http or https
+    server = getenv("N8N_SERVER", Args.instance.wh_server)
+    port = getenv("N8N_PORT",Args.instance.wh_port)
+    endpoint = getenv("N8N_ENDPOINT", wh_endpoint) # this can change for different workflows
+    path = getenv("N8N_PATH", Args.instance.wh_path)
+    conf = getenv("N8N_PRODUCER", Args.instance.n8n_producer)
     Args:
         none
     """
+    
+    producer = configure_n8n_producer()
 
+def configure_n8n_producer(wh_endpoint: str = None) -> dict:
+    """
+    Need to be able to change the endpoint for different workflows and webhooks
+
+    Args:
+        wh_endpoint (str, optional): _description_. Defaults to None.
+
+    Returns:
+        dict: _description_
+    """
     global conf
-    if Args.instance.n8n_producer:
-        conf = Args.instance.n8n_producer
-        producer = conf
-        # good place to do defaults, get api keys, etc
-        logger.debug('N8N producer initialized')
+    conf = None
+    from os import getenv
+    token = getenv("N8N_TOKEN", Args.instance.wh_token)
+    scheme = getenv("N8N_SCHEME",Args.instance.wh_scheme)
+    server = getenv("N8N_SERVER", Args.instance.wh_server)
+    port = getenv("N8N_PORT",Args.instance.wh_port)
+    endpoint = getenv("N8N_ENDPOINT", wh_endpoint)
+    path = getenv("N8N_PATH", Args.instance.wh_path)
+    if token is None or scheme is None or server is None or port is None or path is None:
+        logger.debug('N8N producer not configured in config/Config.py or environment variables')
+        conf = getenv("N8N_PRODUCER", Args.instance.n8n_producer)
+    else:
+        conf = {"authorization": f"Basic {token}", "n8n_url": f'"{scheme}://{server}:{port}/{wh_endpoint if wh_endpoint is not None else endpoint}/{path}"'}
+            
+    return conf
 
-
-def send_n8n_message(http_method: str = "POST", ins_upd_dlt: str = "ins", msg: str = "",
+def send_n8n_message(http_method: str = "POST", 
+        ins_upd_dlt: str = "ins", 
+        msg: str = "",
         wh_entity: str = "unknown",
         logic_row: LogicRow = None, 
         row_dict_mapper: RowDictMapper = None, 
-        payload: dict = None) -> any:
+        payload: dict = None,
+        wh_endpoint: str = None):
     """ Send N8N webhook message regarding [logic_row, mapped by row_dict_mapper or by  payload]
 
     * Typically called from declare_logic event
@@ -64,13 +105,15 @@ def send_n8n_message(http_method: str = "POST", ins_upd_dlt: str = "ins", msg: s
         row_dict_mapper (RowDictMapper): (Optional) typically subclass of RowDictMapper, transforms row to dict
         payload (str): (Optional) JSON data to be sent as string (json.dumps(row.to_dict()))    
         wh_entity (str): the webhook entity name pass in header
+        wh_endpoint (str): the webhook endpoint name modify the producer
     """
 
-    global conf
-    if conf is None:
-        conf = Args.instance.n8n_producer #FIXME not sure why this fails - conf is None
+    #global conf
+    #if conf is None:
+    #    conf = Args.instance.n8n_producer #FIXME not sure why this fails - conf is None
         #return "N8N not enabled in Config.py"
-        
+    conf = configure_n8n_producer(wh_endpoint)
+    logger.debug(f"n8n_producer: conf: {conf}")
     if row_dict_mapper is None and payload is None and logic_row is None:
         return "send_n8n_message: payload, logic_row, row_dict_mapper are all None - must provide one"
     row_obj_dict = None
@@ -96,17 +139,18 @@ def send_n8n_message(http_method: str = "POST", ins_upd_dlt: str = "ins", msg: s
         status = {"status_code": 500}
 
         if conf is None:
-            logger.debug(f"n8n_producer: not configured in config/Config.py")
+            logger.debug(f"n8n_producer.py: N8N not configured in config/Config.py or environment variables")
         else:
             headers = {
                 "Authorization": conf['authorization'],
                 "Content-Type": "application/json",
                 "wh_state": wh_state,
-                "wh_entity": wh_entity
+                "wh_entity": wh_entity,
+                "wh_source": "api_logic_server",
             }
             endpoint = f'{conf["n8n_url"]}'
             status = {"status_code": 500}
-            if http_method in {"post", "POST"}:
+            if http_method.lower() == "post":
                 #Only passing payload in this example
                 status = requests.post(endpoint, json=row_obj_dict, headers=headers)
             elif http_method.lower() == "get":
