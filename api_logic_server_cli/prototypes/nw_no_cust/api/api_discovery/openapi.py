5ebd58ca49872bd3322090d2e170e53ecfc76332
Val Huber <valjhuber@gmail.com>
2025-05-03 06:31:15 -0700

    support other server locations

--- api_logic_server_cli/prototypes/nw_no_cust/api/api_discovery/openapi.py
+++ api_logic_server_cli/prototypes/nw_no_cust/api/api_discovery/openapi.py
@@ -4,19 +4,29 @@ import logging
 import os
 import json
 import io
+from config.config import Args  # circular import error if at top
 
 app_logger = logging.getLogger("api_logic_server_app")
 
 def add_service(app, api, project_dir, swagger_host: str, PORT: str, method_decorators = []):
     pass
 
+    def get_server_url():
+        """ return the server URL for the OpenAPI spec """
+        result = Args.instance.keycloak_base_url
+        # get env variable API_LOGIC_SERVER_TUNNEL (or None)
+        if tunnel_url := os.getenv("API_LOGIC_SERVER_TUNNEL", None):
+            app_logger.info(f".. tunnel URL: {tunnel_url}")
+            result = tunnel_url
+        return result
+        
 
     @app.route('/api/openapi')
     def openapi(path=None):
         """ return integration/openai_plugin/swagger_3.json 
         * with updated tunnel URL if API_LOGIC_SERVER_TUNNEL is set
 
-        test: curl -X GET http://localhost:56565/api/openapi
+        test: curl -X GET http://localhost:5656/api/openapi
         """
             
         # read dict from json file integration/openai_plugin/swagger_3.json
@@ -24,10 +34,8 @@ def add_service(app, api, project_dir, swagger_host: str, PORT: str, method_deco
             swagger_dict = json.load(json_file)
         app_logger.info(f"openapi: Swagger JSON loaded: {swagger_dict}")
 
-        # get env variable API_LOGIC_SERVER_TUNNEL (or None), and set url to tunnel
-        if tunnel_url := os.getenv("API_LOGIC_SERVER_TUNNEL", None):
-            app_logger.info(f".. tunnel URL: {tunnel_url}")
-            swagger_dict["servers"][0]["url"] = tunnel_url
+        server_url = get_server_url()
+        swagger_dict["servers"][0]["url"] = server_url
 
         # convert dict to buffered stream
         swagger_dict_mem = io.BytesIO(json.dumps(swagger_dict).encode('utf-8'))
@@ -39,7 +47,7 @@ def add_service(app, api, project_dir, swagger_host: str, PORT: str, method_deco
         """ return integration/openai_plugin/ai_plugin.json 
         * with updated tunnel URL if API_LOGIC_SERVER_TUNNEL is set
 
-        test: curl -X GET http://localhost:56565/api/ai_plugin
+        test: curl -X GET http://localhost:5656/api/ai_plugin
         """
 
         # read dict from json file integration/openai_plugin/swagger_3.json
@@ -47,10 +55,8 @@ def add_service(app, api, project_dir, swagger_host: str, PORT: str, method_deco
             swagger_dict = json.load(json_file)
         app_logger.info(f"openapi: ai_plugin JSON loaded: {swagger_dict}")
 
-        # get env variable API_LOGIC_SERVER_TUNNEL (or None), and set url to tunnel
-        if tunnel_url := os.getenv("API_LOGIC_SERVER_TUNNEL", None):
-            app_logger.info(f".. tunnel URL: {tunnel_url}")
-            swagger_dict["api"]["url"] = tunnel_url
+        server_url = get_server_url()
+        swagger_dict["servers"][0]["url"] = server_url
 
         # convert dict to buffered stream
         swagger_dict_mem = io.BytesIO(json.dumps(swagger_dict).encode('utf-8'))
