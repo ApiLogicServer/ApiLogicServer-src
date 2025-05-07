3bda478876cbd91df14846970ee88027ba74d178
Val Huber <valjhuber@gmail.com>
2025-05-06 17:41:36 -0700

    setup procedures

--- api_logic_server_cli/prototypes/nw_no_cust/integration/mcp/3_executor_test_agent.py
+++ api_logic_server_cli/prototypes/nw_no_cust/integration/mcp/3_executor_test_agent.py
@@ -26,9 +26,20 @@ tool_context = {
     }
 }
 
+tool_context = {  # use this for nw
+    "method": "GET",
+    "url": "http://localhost:5656/api/Customer",
+    "query_params": {
+        "filter[name]": "Alice"
+    },
+    "headers": {
+        "Accept": "application/vnd.api+json"
+        , "Authorization": "Bearer your_token"
+    }
+}
 
 # Execute as a simulated MCP executor
-response = requests.get(
+response = requests.get(  # use this for genai_demo
     tool_context["url"],
     headers=tool_context["headers"],
     params=tool_context["query_params"]
