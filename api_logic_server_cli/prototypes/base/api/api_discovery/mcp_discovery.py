162495544ffe84246cc971166f6e22f1ba6b96c0
Val Huber <valjhuber@gmail.com>
2025-06-06 19:53:46 -0700

    log cleanup

--- api_logic_server_cli/prototypes/base/api/api_discovery/mcp_discovery.py
+++ api_logic_server_cli/prototypes/base/api/api_discovery/mcp_discovery.py
@@ -1,9 +1,8 @@
 from flask import request, jsonify
-from flask import Flask, redirect, send_from_directory, send_file
 import logging
 import os
 import json
-import io
+from pathlib import Path
 
 import requests
 from config.config import Args  # circular import error if at top
@@ -23,36 +22,76 @@ def add_service(app, api, project_dir, swagger_host: str, PORT: str, method_deco
         return result  #  + '/api'
         
 
+    @app.before_request
+    def before_any_request():
+        # print(f"[DEBUG] Incoming request: {request.method} {request.url}")
+        if activate_openapi_logging := False:
+            if request.content_type == 'application/json' and request.method in ['POST', 'PUT', 'PATCH']:
+                # openapi: Incoming request: PATCH http://localhost:5656/api/Customer/1/ {'data': {'attributes': {'credit_limit': 5555}, 'type': 'Customer', 'id': '1'}}
+                # openapi: Incoming request: PATCH http://6f6f-2601-644-4900-d6f0-ecc9-6df3-8863-c5b2.ngrok-free.app/api/Customer/1 {'credit_limit': 5555}
+
+                app_logger.info(f"openapi: Incoming request: {request.method} {request.url} {str(request.json)}")
+            else:
+                app_logger.info(f"openapi: Incoming request: {request.method} {request.url}")
+            # app_logger.info(f"openapi: Incoming request headers: {request.headers}")
+
+            chatgpt_request_json = {
+                        "credit_limit": 25000,
+            }
+            standard_request_json = {
+                "data": {
+                    "type": "Customer",
+                    "id": "ALFKI",
+                    "attributes": {
+                        "name": "Alice",
+                        "credit_limit": 25000,
+                        "balance": 12345
+                    }
+                }
+            }
+            swagger_request_json = {
+                'data': {
+                    'attributes': {
+                        'credit_limit': 5555
+                        }, 
+                    'type': 'Customer', 
+                    'id': '1'
+                }
+            }
+        pass
+    
 
     @app.route('/.well-known/mcp.json', methods=['GET'])
     def mcp_discovery(path=None):
-        ''' called by mcp_client_executor for discovery, eg:
-        ```
-        {
-            "tool_type": "json-api",
-            "schema_version": "1.0",
-            "base_url": "https://crm.company.com",
-            "resources": [
-                {
-                "name": "Customer",
-                "path": "/Customer",
-                "methods": ["GET", "PATCH"],
-                "fields": ["id", "name", "balance", "credit_limit"],
-                "filterable": ["name", "credit_limit"],
-                "example": "List customers with credit over 5000"
-                }
-            ]
-        }
-        ```
-        test: curl -X GET "http://localhost:5656/.well-known/mcp.json"
+        ''' called by mcp_client_executor for discovery; read docs/mcp_learning/ for:
+        1. learning, including fan-out & response format, and email requests.
+        2. schema
+
+        see: https://apilogicserver.github.io/Docs/Integration-MCP/#1-discovery
+
+            test: curl -X GET "http://localhost:5656/.well-known/mcp.json"
         '''
         # return docs/mcp_schema.json
-        schema_path = os.path.join(project_dir, "docs/mcp_learning/mcp_schema.json")
+        mcp_learning_path = Path(project_dir + "/docs/mcp_learning")
         try:
+            schema_path = mcp_learning_path.joinpath('mcp_schema.json')
+            if not schema_path.exists():
+                return jsonify({"error": "System Error - /docs/mcp_learning/mcp_schema.json not found"}), 404
             with open(schema_path, "r") as schema_file:
                 schema = json.load(schema_file)
-                return jsonify(schema), 200
         except Exception as e:
             app_logger.error(f"Error loading MCP schema: {e}")
             return jsonify({"error": "MCP schema not found"}), 404
-        pass
+
+        try:
+            learnings_path = mcp_learning_path.joinpath('mcp.prompt')
+            if not learnings_path.exists():
+                return jsonify({"error": "System Error - /docs/mcp_learning/mcp,prompt not found"}), 404
+            with open(learnings_path, "r") as learnings_file:
+                learnings = learnings_file.read()
+        except Exception as e:
+            app_logger.error(f"Error loading MCP learnings: {e}")
+            return jsonify({"error": "MCP learnings not found"}), 404
+        schema['learning'] = learnings
+
+        return jsonify(schema), 200
