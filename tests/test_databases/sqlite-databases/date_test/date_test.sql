6e07efe9a83f89ca657c3ceea21db60f23d97def
Val Huber <valjhuber@gmail.com>
2025-07-09 10:05:44 -0700

    recreated bug

--- tests/test_databases/sqlite-databases/date_test/date_test.sql
+++ tests/test_databases/sqlite-databases/date_test/date_test.sql
@@ -1,12 +1,16 @@
 -- sqlite3 date_test.sqlite < date_test.sql
 -- genai-logic create  --project_name=date_test --db_url=sqlite:///date_test.sqlite
--- 
+--    Rule.formula(derive=models.Employee.age,  as_exp=lambda row: int((datetime.datetime.now() - row.birth_date).days / 365.25) if row.birth_date else None)
+
+-- Observe: Invalid Rules:  [AttributeError("'function' object has no attribute 'strip'")]
+
 
 CREATE TABLE Employee (
         id INTEGER NOT NULL, 
         name VARCHAR, 
         email varchar,
         email_opt_out BOOLEAN,
-        birth_date datetime,
+        birth_date date,
+        age int,
         PRIMARY KEY (id)
 );
