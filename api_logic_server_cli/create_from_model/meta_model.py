e74317f21a798d9fcff9a60092266d1b99e92fb2
Tyler Band <tylerm007@gmail.com>
2025-07-27 13:24:52 -0400

    fix for computed arg attribute (Shouki)

--- api_logic_server_cli/create_from_model/meta_model.py
+++ api_logic_server_cli/create_from_model/meta_model.py
@@ -42,7 +42,7 @@ class ResourceAttribute():
             if lower_name.endswith(each_non_fav):
                 self.non_favorite = True
                 break
-        if each_attribute.server_default is not None and hasattr(each_attribute.server_default.arg, 'text'):
+        if each_attribute.server_default is not None and "arg" in dir(each_attribute.server_default) and hasattr(each_attribute.server_default.arg, 'text'):
             self.default = each_attribute.server_default.arg.text
         resource.attributes.append(self)
 
