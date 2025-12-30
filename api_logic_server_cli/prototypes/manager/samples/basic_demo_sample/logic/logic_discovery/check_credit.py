039e0bbc3e9667a6ee0ab73ab3f90769cab1d1a2
Val Huber <valjhuber@gmail.com>
2025-12-30 08:52:34 -0800

    demo sizing

--- api_logic_server_cli/prototypes/manager/samples/basic_demo_sample/logic/logic_discovery/check_credit.py
+++ api_logic_server_cli/prototypes/manager/samples/basic_demo_sample/logic/logic_discovery/check_credit.py
@@ -24,24 +24,8 @@ def declare_logic():
     5. The Item unit_price is copied from the Product unit_price
     """
     
-    # 1. The Customer's balance is less than the credit limit
-    Rule.constraint(validate=models.Customer,
-                   as_condition=lambda row: row.balance <= row.credit_limit,
-                   error_msg="Customer balance exceeds credit limit")
-    
-    # 2. The Customer's balance is the sum of the Order amount_total where date_shipped is null
-    Rule.sum(derive=models.Customer.balance, 
-             as_sum_of=models.Order.amount_total,
-             where=lambda row: row.date_shipped is None)
-    
-    # 3. The Order's amount_total is the sum of the Item amount
-    Rule.sum(derive=models.Order.amount_total, 
-             as_sum_of=models.Item.amount)
-    
-    # 4. The Item amount is the quantity * unit_price
-    Rule.formula(derive=models.Item.amount, 
-                 as_expression=lambda row: row.quantity * row.unit_price)
-    
-    # 5. The Item unit_price is copied from the Product unit_price
-    Rule.copy(derive=models.Item.unit_price, 
-              from_parent=models.Product.unit_price)
+    Rule.constraint(validate=models.Customer, as_condition=lambda row: row.balance <= row.credit_limit, error_msg="Customer balance exceeds credit limit")                    
+    Rule.sum(derive=models.Customer.balance, as_sum_of=models.Order.amount_total, where=lambda row: row.date_shipped is None)    
+    Rule.sum(derive=models.Order.amount_total, as_sum_of=models.Item.amount)
+    Rule.formula(derive=models.Item.amount, as_expression=lambda row: row.quantity * row.unit_price)
+    Rule.copy(derive=models.Item.unit_price, from_parent=models.Product.unit_price)
