13b95a4a0250a656c216c445dc2265dc4bf83b25
Val Huber <valjhuber@gmail.com>
2026-02-10 20:16:59 -0800

    v bump

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "16.01.24"  # last public release: 16.00.09
+__version__ = "16.01.25"  # last public release: 16.00.09
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t02/10/2026 - 16.01.24: sample-rework \n"\
+    "\t02/10/2026 - 16.01.25: sample-rework \n"\
     "\t02/06/2026 - 16.01.22: save nl logic by use-case/reqmt, logic operation, docent, demo fix \n"\
     "\t01/06/2026 - 16.01.03: win11 Python 3.13 fixes for panda, oracle, kafka, postgres \n"\
     "\t12/11/2025 - 16.00.04: bug fix [106] - SqlServer autoinsert \n"\
