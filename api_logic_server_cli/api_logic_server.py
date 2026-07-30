dd30a8e2fe72013b30b1ed38b4f5362bf2ed30fb
Val Huber <valjhuber@gmail.com>
2026-07-30 13:11:07 -0700

    v#

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,9 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "17.03.06"  # last public release: 17.03.01
+__version__ = "17.03.07"  # last public release: 17.03.06
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
+    "\t07/30/2026 - 17.03.07  Context Engineering for Dashboards & Vibe, LB and CS fixes / examples \n"\
     "\t07/22/2026 - 17.03.06  Context Engineering for Dashboards & Vibe, LB and CS fixes \n"\
     "\t07/10/2026 - 17.02.08  entity types diagram, LB 1.31.05 evt bug, admin home page fixes, simple browser (mgr), count inits \n"\
     "\t06/11/2026 - 17.00.35  logic report / flow diagram, cvls with hazmat, basic_demo_logic_gov, sec & raw logic fixes \n"\
